d7de90a9946add4384263188382edce4ef80f86f
ezequielvera11 <ezevc11@gmail.com>
2023-09-01 20:21:45 -0300

    Solución
    El evento click del div no se ejecuta junto con el del botón.

--- eventos.js
+++ eventos.js
@@ -1,5 +1,18 @@
+//Creamos el DOM para que primero cargue el documento.
+//Agregamos una constante que llame al div y otra al boton.
+//Creamos un evento que cuando clickeamos el div despliegue un alert
+//Creamos un evento que cuando clickeamos el boton detiene el evento del div y despliegue un alert.
+
 document.addEventListener("DOMContentLoaded", function(){
     const div = document.querySelector("div");
+    const boton = document.querySelector("button");
 
     div.addEventListener("click", () => {alert("Hola! Soy el div")});
+
+    boton.addEventListener("click", (event) => {
+        event.stopPropagation()
+        alert("Hola!")
+    });
+
+
 });
